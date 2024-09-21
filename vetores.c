1f6196071ff7186c0ff61e3b33fe6793d1da4719
ODiogorocha <135875382+ODiogorocha@users.noreply.github.com>
2024-09-21 15:56:33 -0300

    vetores.c

--- vetores.c
+++ vetores.c
@@ -87,45 +87,78 @@ void tela_geral(){
     int valor_de_selecao = 0;
     int vetor[5]; 
     int media;
+    int vetor_preenchido = 0;
 
-    printf("Voce gostaria de acessar:\n");
-    printf("ler vetor (1)\n");
-    printf("inverter vetor (2)\n");
-    printf("ordenar o vetor (3)\n");
-    printf("maior valor do vetor (4)\n");
-    printf("menor valor do vetor (5)\n");
-    printf("media dos valores (6)\n");
-    printf("valores maiores que a media (7)\n");
+    while (1) {  
+        printf("\nVoce gostaria de acessar:\n");
+        printf("1 - Ler vetor\n");
+        printf("2 - Inverter vetor\n");
+        printf("3 - Ordenar o vetor\n");
+        printf("4 - Maior valor do vetor\n");
+        printf("5 - Menor valor do vetor\n");
+        printf("6 - Media dos valores\n");
+        printf("7 - Valores maiores que a media\n");
+        printf("0 - Sair\n");
 
-    scanf("%d", &valor_de_selecao); 
+        printf("Escolha uma opcao: ");
+        scanf("%d", &valor_de_selecao); 
 
-    switch (valor_de_selecao){
-    case 1:
-        le_vetor_de_inteiros(vetor);
-        break;
-    case 2:
-        inverte_vetor(vetor);
-        break;
-    case 3:
-        ordenar_vetor(vetor);
-        break;
-    case 4:
-        printf("Maior valor: %d\n", maior_valor_do_vetor(vetor));
-        break;
-    case 5:
-        printf("Menor valor: %d\n", menor_valor_do_vetor(vetor));
-        break;
-    case 6:
-        media = media_de_valores_de_vetor(vetor);
-        printf("Media: %d\n", media);
-        break;
-    case 7:
-        media = media_de_valores_de_vetor(vetor); 
-        printf("Valores maiores que a media: %d\n", valores_maiores_que_a_media(vetor, media));
-        break;
-    default:
-        printf("Opcao invalida!\n");
-        break;
+        switch (valor_de_selecao){
+        case 1:
+            le_vetor_de_inteiros(vetor);
+            vetor_preenchido = 1;  
+            break;
+        case 2:
+            if (vetor_preenchido) {
+                inverte_vetor(vetor);
+            } else {
+                printf("O vetor ainda nao foi preenchido.\n");
+            }
+            break;
+        case 3:
+            if (vetor_preenchido) {
+                ordenar_vetor(vetor);
+            } else {
+                printf("O vetor ainda nao foi preenchido.\n");
+            }
+            break;
+        case 4:
+            if (vetor_preenchido) {
+                printf("Maior valor: %d\n", maior_valor_do_vetor(vetor));
+            } else {
+                printf("O vetor ainda nao foi preenchido.\n");
+            }
+            break;
+        case 5:
+            if (vetor_preenchido) {
+                printf("Menor valor: %d\n", menor_valor_do_vetor(vetor));
+            } else {
+                printf("O vetor ainda nao foi preenchido.\n");
+            }
+            break;
+        case 6:
+            if (vetor_preenchido) {
+                media = media_de_valores_de_vetor(vetor);
+                printf("Media: %d\n", media);
+            } else {
+                printf("O vetor ainda nao foi preenchido.\n");
+            }
+            break;
+        case 7:
+            if (vetor_preenchido) {
+                media = media_de_valores_de_vetor(vetor); 
+                printf("Valores maiores que a media: %d\n", valores_maiores_que_a_media(vetor, media));
+            } else {
+                printf("O vetor ainda nao foi preenchido.\n");
+            }
+            break;
+        case 0:
+            printf("Encerrando o programa...\n");
+            return; 
+        default:
+            printf("Opcao invalida!\n");
+            break;
+        }
     }
 }
 
