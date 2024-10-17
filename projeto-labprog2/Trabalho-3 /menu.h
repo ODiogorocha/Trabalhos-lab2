0e41d8b7cd2f2eeea006890977cca7d0dd4d8fc5
WeslleyHBM <126731299+WeslleyHBM@users.noreply.github.com>
2024-10-17 11:51:40 -0300

    Create menu.h

---
+++ projeto-labprog2/Trabalho-3 /menu.h
@@ -0,0 +1,8 @@
+#ifndef MENU_H
+#define MENU_H
+
+void texto_menu(); // função que mostra na tela as orientações do menu.
+
+void menu(int opicao); // Função que opera a partir de um valor recebido, utilizando de um switch case para tomar uma decisão.
+
+#endif
