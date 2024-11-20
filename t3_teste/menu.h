e5672abb252450f16c44da75727e00f87b4294b7
ODiogorocha <diogorocharsrs@gmail.com>
2024-11-20 10:47:55 -0300

    remocao t3 teste

--- t3_teste/menu.h
+++
@@ -1,8 +0,0 @@
-#ifndef MENU_H
-#define MENU_H
-
-#include "lista.h"
-
-void menu(int opcao, Alunos** lista_alunos, Professores** lista_professores, Projetos** lista_projetos);  // Declaração da função menu
-
-#endif // MENU_H
