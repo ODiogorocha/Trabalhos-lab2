bd870c5a2e504c2537d075f060a2b1b3feeff254
WeslleyHBM <126731299+WeslleyHBM@users.noreply.github.com>
2024-12-08 19:52:14 -0300

    Update pilha.h

--- projeto-labprog2/Trabalho-4/pilha.h
+++ projeto-labprog2/Trabalho-4/pilha.h
@@ -1,25 +1,24 @@
 #ifndef PILHA_H
 #define PILHA_H
-
 #include <stdbool.h>
 
-// Estrutura para representar um prato na pilha
-typedef struct NoPilha {
-    int prato_id;
-    struct NoPilha* proximo;
-} NoPilha;
+typedef struct pratos Pratos;
+typedef struct pilha Pilha;
 
-typedef struct {
-    NoPilha* topo;
-    int tamanho;
-} Pilha;
+struct pratos {
+    bool prato_limpo;
+};
 
-// Funções para gerenciamento da pilha
-Pilha* inicializar_pilha();
-void empilhar_prato(Pilha* pilha, int prato_id);
-int desempilhar_prato(Pilha* pilha);
-bool pilha_vazia(Pilha* pilha);
-void imprimir_pilha(Pilha* pilha);
-void destruir_pilha(Pilha* pilha);
 
-#endif 
+struct pilha {
+    Pratos prato;
+    Pilha *prox;
+};
+
+Pilha* inicia_pilha();
+void empilhar(Pilha **pilha, int quantidade);
+void desempilhar(Pilha **pilha, int quantidade);
+void imprimir_pilha(Pilha *pilha);
+void destruir_pilha(Pilha **inicio);
+
+#endif
